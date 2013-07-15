General fixes, added elastic impacts, added derivatives for root-solvers, added sphere impact surface, added bouncing example to show non-plastic impacts.

src/impact.py
@@ -23,36 +23,61 @@ def linear_interpolate_impact(mvi):
 
 
 def solve_impact_time(mvi):
-    """ Finds the time and configuration at impact. """
+    """ Finds the time and configuration at impact.
+          when passed to this function, mvi should have following states:
+            0 - two states before impact
+            1 - state just before impact
+            2 - invalid configuration to be corrected
+    """
 
-    n, m, nk = mvi.nd, mvi.nc, mvi.nk
+    nd, m = mvi.nd, mvi.nc
 
     # Linearly interpolate the kinematic configuration at the moment of impact.
-    qk1, qk2 = mvi.q1[n:], mvi.q2[n:]
+    qk1, qk2 = mvi.q1[nd:], mvi.q2[nd:]
     def kin_config(t):
         return (t-mvi.t1)/(mvi.t_end-mvi.t1)*(qk2-qk1)+qk1
 
     set_system_state(mvi, 1)
-    Dh1  = Dh(mvi)
-    Dh1T = np.transpose(Dh1)
+    Dh1T = np.transpose(Dh(mvi))
 
     def func(x):
                 
         mvi.t2 = x[-1]
-        mvi.q2 = np.append(x[:n], kin_config(x[-1]))
-        mvi.lambda1 = x[(n+nk):(n+nk+m)]
+        mvi.q2 = np.append(x[:nd], kin_config(x[-1]))
+        mvi.lambda1 = x[nd:(nd+m)]
 
         mvi.set_midpoint()
-        f1 = mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T[:n],mvi.lambda1)
+        f1 = mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T,mvi.lambda1)
 
         set_system_state(mvi, 2)
         f2 = h(mvi)
         f3 = phi(mvi)
+
         return np.hstack((f1, f2, f3))
 
+    def fprime(x):
+        mvi.t2 = x[-1]
+        mvi.q2 = np.append(x[:nd], kin_config(x[-1]))
+        mvi.lambda1 = x[(nd):(nd+m)]
+        
+        mvi.set_midpoint()
+        Df11 = D2D1L2(mvi) + D2fm2(mvi)
+        Df13 = D4D1L2(mvi) + D4fm2(mvi)
+        
+        set_system_state(mvi, 2)
+        Df21 = Dh(mvi)
+        Df31 = Dphi(mvi)
+        Df23 = D2h(mvi).reshape((mvi.nc, 1))
+
+        Df1 = np.hstack((Df11, -Dh1T, Df13))
+        Df2 = np.hstack((Df21, np.zeros((mvi.nc, mvi.nc)), Df23))
+        Df3 = np.hstack((Df31, np.zeros(mvi.nc+1)))
+
+        return np.vstack((Df1, Df2, Df3))
+        
     t_guess, q_guess = linear_interpolate_impact(mvi)
-    x0 = np.hstack((q_guess[:n], mvi.lambda1, t_guess))
-    (x, infodict, ier, mesg) = scipy.optimize.fsolve(func, x0, full_output=True)
+    x0 = np.hstack((q_guess[:nd], mvi.lambda1, t_guess))
+    (x, infodict, ier, mesg) = scipy.optimize.fsolve(func, x0,full_output=True, fprime=fprime)
 
     if ier != 1:
         # The root solver will fail for exactly simultaneous collisions since setting
@@ -60,11 +85,11 @@ def solve_impact_time(mvi):
         # but can at least check for it.
         if mvi.t1 == x[-1]:
             print 'Simultaneous impacts detected.'
-        raise trep.ConvergenceError("Find impact failed to converge.")      
+        raise trep.ConvergenceError("Find impact failed to converge.")           
 
-    mvi.q2 = np.append(x[:n], kin_config(x[-1]))
+    mvi.q2 = np.append(x[:nd], kin_config(x[-1]))
     mvi.t2 = x[-1]
-    mvi.lambda1 = x[(n+nk):(n+nk+m)]
+    mvi.lambda1 = x[nd:(nd+m)]
     mvi.set_midpoint()    
     mvi.p2 = D2L2(mvi)
 
@@ -72,9 +97,16 @@ def solve_impact_time(mvi):
 
 
 def plastic_impact(mvi):
-    """ Finds the time and configuration at impact. """
+    """ Finds the configuration after impact.
+          when passed to this function, mvi should have following states
+            0 - two states before impact
+            1 - state just before impact
+            2 - impact state
+    """
+    nd, m = mvi.nd, mvi.nc
 
-    n, m, nk = mvi.nd, mvi.nc, mvi.nk
+    mvi.set_midpoint()
+    tau = D4L2(mvi)
 
     mvi.set_single_state(0, mvi.t1, mvi.q1, mvi.p1)
     mvi.set_single_state(1, mvi.t2, mvi.q2, mvi.p2)
@@ -82,43 +114,133 @@ def plastic_impact(mvi):
     mvi.lambda0 = mvi.lambda1
 
     set_system_state(mvi, 1)
-    Dh1T = np.transpose(Dh(mvi))
-    Dphi1 = Dphi(mvi)
+    Dh1T  = np.transpose(Dh(mvi))
+    Dphi1 = np.transpose(Dphi(mvi))
     D2h1  = D2h(mvi) 
 
-    mvi.set_midpoint()
-    tau = D4L2(mvi)
-
     def func(x):
-        mvi.q2 = np.append(x[:n], mvi.qk_end)
-        mvi.lambda1 = x[(n+nk):(n+nk+m)]
+        mvi.q2 = np.append(x[:nd], mvi.qk_end)
+        mvi.lambda1 = x[nd:(nd+m)]
         lambda_c = x[-2]
-        Ec = x[-1]
+        Ec = x[-1]        
 
         set_system_state(mvi, 2)
         f2 = h(mvi)
         f4 = phi(mvi)
 
         mvi.set_midpoint()
-        f1 = (mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T[:n],mvi.lambda1)
-              -np.transpose(Dphi1)*lambda_c)[0]
-        f3 =  (tau+D3L2(mvi)-np.dot(D2h1,mvi.lambda1)+Ec)
+        f1 = mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T,mvi.lambda1)-np.transpose(Dphi1)*lambda_c
+        f3 = tau+D3L2(mvi)-np.dot(D2h1, mvi.lambda1)+Ec
+
         return np.hstack((f1, f2, f3, f4))
 
-    x0 = np.hstack((mvi.q2[:n], mvi.lambda1, 0.0, 0.0))
-    (x, infodict, ier, mesg) = scipy.optimize.fsolve(func, x0, full_output=True)
+    def fprime(x):
+        mvi.q2 = np.append(x[:nd], mvi.qk_end)
+        mvi.lambda1 = x[nd:nd+m]
+        lambda_c = x[-2]
+        Ec = x[-1] 
+
+        mvi.set_midpoint()
+        Df11 = (D2D1L2(mvi) + D2fm2(mvi))
+        Df31 = D2D3L2(mvi)
+
+        set_system_state(mvi, 2)
+        Df21 = Dh(mvi)
+        Df41 = Dphi(mvi)
+        Df32 = -D2h(mvi)
+
+        Df1 = np.column_stack((Df11, -Dh1T, Dphi1, np.zeros((nd,1))))
+        Df2 = np.hstack((Df21, np.zeros((mvi.nc, mvi.nc+2))))
+        Df3 = np.hstack((Df31, Df32, (0.0, 1.0)))
+        Df4 = np.hstack((Df41,  np.zeros(mvi.nc+2)))
+
+        return np.vstack((Df1, Df2, Df3, Df4))
+
+    x0 = np.hstack((mvi.q2[:nd], mvi.lambda1, np.dot(Dphi1, mvi.p1), 0.0))
+    (x, infodict, ier, mesg) = scipy.optimize.fsolve(func, x0, full_output=True, fprime=fprime)
 
     if ier != 1:
-        raise trep.ConvergenceError("Impact update failed to converge.")
+        print mesg
+        raise trep.ConvergenceError("Plastic impact update failed to converge.")  
 
-    mvi.q2 = np.append(x[:n],mvi.qk_end)
-    mvi.lambda1 = x[(n+nk):(n+nk+m)]
+    mvi.q2 = np.append(x[:nd],mvi.qk_end)
+    mvi.lambda1 = x[nd:(nd+m)]
+    mvi.set_midpoint()    
+    mvi.p2 = D2L2(mvi)
+
+    return {'t': mvi.t2, 'q': mvi.q2, 'lambda1': mvi.lambda1, 'lambdac': x[-2], 'Ec': x[-1]}
+
+def elastic_impact(mvi, Ec):
+    """ Finds the configuration after impact.
+          when passed to this function, mvi should have following states
+            0 - two states before impact
+            1 - state just before impact
+            2 - impact state
+    """
+    nd, m = mvi.nd, mvi.nc
+
+    mvi.set_midpoint()
+    tau = D4L2(mvi)
+
+    mvi.set_single_state(0, mvi.t1, mvi.q1, mvi.p1)
+    mvi.set_single_state(1, mvi.t2, mvi.q2, mvi.p2)
+    mvi.t2 = mvi.t_end
+    mvi.lambda0 = mvi.lambda1
+
+    set_system_state(mvi, 1)
+    Dh1T  = np.transpose(Dh(mvi))
+    Dphi1 = np.transpose(Dphi(mvi))
+    D2h1  = D2h(mvi) 
+
+    def func(x):
+        mvi.q2 = np.append(x[:nd], mvi.qk_end)
+        mvi.lambda1 = x[nd:(nd+m)]
+        lambda_c = x[-1]      
+
+        set_system_state(mvi, 2)
+        f2 = h(mvi)
+ 
+        mvi.set_midpoint()
+        f1 = mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T,mvi.lambda1)-np.transpose(Dphi1)*lambda_c
+        f3 = tau+D3L2(mvi)-np.dot(D2h1, mvi.lambda1)+Ec
+
+        return np.hstack((f1, f2, f3))
+
+    def fprime(x):
+        mvi.q2 = np.append(x[:nd], mvi.qk_end)
+        mvi.lambda1 = x[nd:(nd+m)]
+        lambda_c = x[-2]
+        Ec = x[-1] 
+
+        mvi.set_midpoint()
+        Df11 = D2D1L2(mvi)+D2fm2(mvi)
+        Df31 = D2D3L2(mvi)
+
+        set_system_state(mvi, 2)
+        Df21 = Dh(mvi)
+        Df32 = -D2h(mvi)
+
+        Df1 = np.column_stack((Df11, -Dh1T, Dphi1))
+        Df2 = np.hstack((Df21, np.zeros((mvi.nc, mvi.nc+1))))
+        Df3 = np.hstack((Df31, Df32, (0.0)))
+
+        return np.vstack((Df1, Df2, Df3))
+
+    x0 = np.hstack((mvi.q0[:nd], mvi.lambda1, np.dot(Dphi1, mvi.p1)))
+    (x, infodict, ier, mesg) = scipy.optimize.fsolve(func, x0, full_output=True, fprime=fprime)
+
+    if ier != 1:
+        print mesg
+        raise trep.ConvergenceError("Elastic impact update failed to converge.")  
+
+    mvi.q2 = np.append(x[:nd],mvi.qk_end)
+    mvi.lambda1 = x[nd:(nd+m)]
     mvi.set_midpoint()    
     mvi.p2 = D2L2(mvi)
 
     return {'t': mvi.t2, 'q': mvi.q2, 'lambda1': mvi.lambda1, 'lambdac': x[-2], 'Ec': x[-1]}
     
-
+ 
 def solve_impact(mvi, impact_frames):
     """ When given one or more impacts, finds the first impact to occur and the state
         at that point. Returns the frame and state. """
@@ -146,15 +268,36 @@ def impact_update(mvi, impact_frame, t, q, p, lam0, lam1):
     # Set system config to impact config. Solve impact update.
     mvi.t2, mvi.q2, mvi.p2, mvi.lambda0, mvi.lambda1 = t, q, p, lam0, lam1
     mvi.surface.set_frame(impact_frame)
-    next_state = plastic_impact(mvi)
-    state = save_state(mvi)
 
-    # Add the constraint to the system.
-    mvi.constrained_frames.append(impact_frame)
-    mvi.surface.add_constraint_to_system(impact_frame)
+    # Perfectly inelastic.
+    if mvi.cor == 0:
+        next_state = plastic_impact(mvi)
+        state = save_state(mvi)
 
-    # Adding constraints can sometimes clear the inteagrator's states. Here, we restore the
-    #   current state. 
-    restore_state(mvi, state)
-    mvi.lambda0 = np.append(mvi.lambda0, [0.0]) 
-    mvi.lambda1 = np.append(next_state['lambda1'], next_state['lambdac'])           
+        # Add the constraint to the system.
+        mvi.constrained_frames.append(impact_frame)
+        mvi.surface.add_constraint_to_system(impact_frame)
+
+        # Adding constraints can sometimes clear the inteagrator's states. Here, we restore the
+        #   current state. 
+        restore_state(mvi, state)
+        mvi.lambda0 = np.append(mvi.lambda0, [0.0]) 
+        mvi.lambda1 = np.append(next_state['lambda1'], next_state['lambdac']) 
+
+    # Perfectly elastic.
+    elif mvi.cor == 1:
+        elastic_impact(mvi, 0.0)
+    
+    # CoR between 0 and 1; need to solve twice.
+    else:    
+        next_state = plastic_impact(mvi)
+        Ec_prime = mvi.cor*next_state['Ec']
+        
+        # At this point it might be nice to check if the energy lost is 
+        # small enough to treat the impact as plastic.
+
+        mvi.set_single_state(2, mvi.t1, mvi.q1, mvi.p1)
+        mvi.set_single_state(1, mvi.t0, mvi.q0, mvi.p0)
+        mvi.lambda1 = mvi.lambda0
+
+        elastic_impact(mvi, Ec_prime)              

src/release.py
@@ -38,9 +38,13 @@ def linear_interpolate_release(mvi, j):
  
 
 def root_solve_release(mvi, j):
-    """ Solves for the state of the system at the time of release of constraint j. """
+    """ Solves for the state of the system at the time of release of constraint j. 
+          when passed to this function, lambda0 and lambda1 will have opposite signs.
+          the release could have occurred anywhere between states 0 and 2, so we
+          will adjust state 1 to match the point of release.
+    """
 
-    n, m = mvi.nd, mvi.nc
+    nd, m, nk = mvi.nd, mvi.nc, mvi.nk
     con = mvi.system.constraints[j]
 
     t1, p1, q1 = mvi.t1, mvi.p1, mvi.q1
@@ -54,31 +58,64 @@ def root_solve_release(mvi, j):
     if mvi.lambda1[j] >= 0:
         opt_sign = 1.0
     else:
-        opt_sign = -1.0  
+        opt_sign = -1.0 
+        
+    qk1, qk2 = mvi.q1[nd:], mvi.q2[nd:]
+    def kin_config(t):
+        return (t-mvi.t1)/(mvi.t_end-mvi.t1)*(qk2-qk1)+qk1
+     
 
     # A built-in scipy solver is used. The goal is to maximize lambda_j while satisfying the dynamics.
     def func(x):
-        return opt_sign*x[n+j]
+        return opt_sign*x[nd+j]
 
     def fprime(x):
         fp = np.zeros(len(x))
-        fp[n+j] = opt_sign*1.0             
+        fp[nd+j] = opt_sign*1.0             
         return fp
 
-    def f_eqcons(x):
-        mvi.q2, mvi.t2, mvi.lambda0 = x[:n], x[-1], x[n:(n+m)] 
-        f = np.zeros(m+n)
-        mvi.system.set_state(q=x[:n], t=x[-1])
-        f2 = h(mvi)
-        mvi.set_midpoint()
-        f1 = (mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T[:n],mvi.lambda0))
-        return np.hstack((f1, f2))
+    def f_eqcons(x):        
+        
+        mvi.t2 = x[-1]
+        mvi.q2 = np.append(x[:nd], kin_config(x[-1]))
+        mvi.lambda1 = x[(nd+nk):(nd+nk+m)]
 
-    bounds = ([(-10*abs(q),10*abs(q)) for q in mvi.q2[:n]] +
+        mvi.set_midpoint()
+        f1 = mvi.p1+D1L2(mvi)+fm2(mvi)-np.dot(Dh1T,mvi.lambda1)
+
+        set_system_state(mvi, 2)
+        f2 = h(mvi)
+        f3 = phi(mvi)       # still on surface before release
+
+        return np.hstack((f1, f2, f3))
+
+    def fprime_eqcons(x):
+
+        mvi.t2 = x[-1]
+        mvi.q2 = np.append(x[:nd], kin_config(x[-1]))
+        mvi.lambda1 = x[(nd):(nd+m)]
+        
+        mvi.set_midpoint()
+        Df11 = D2D1L2(mvi) + D2fm2(mvi)
+        Df13 = D4D1L2(mvi) + D4fm2(mvi)
+        
+        set_system_state(mvi, 2)
+        Df21 = Dh(mvi)
+        Df31 = Dphi(mvi)
+        Df23 = D2h(mvi).reshape((mvi.nc, 1))
+
+        Df1 = np.hstack((Df11, -Dh1T, Df13))
+        Df2 = np.hstack((Df21, np.zeros((mvi.nc, mvi.nc)), Df23))
+        Df3 = np.hstack((Df31, np.zeros(mvi.nc+1)))
+
+        return np.vstack((Df1, Df2, Df3))
+    
+
+    bounds = ([(-10*abs(q),10*abs(q)) for q in mvi.q2[:nd]] +
               [(-10*abs(l),10*abs(l)) for l in mvi.lambda1] + [(mvi.t0,mvi.t2)])    
 
     x0 = np.hstack((q1, mvi.lambda0, t1))
-    (x_opt, fx, its, imode, smode) = scipy.optimize.fmin_slsqp(func, x0, f_eqcons=f_eqcons,
+    (x_opt, fx, its, imode, smode) = scipy.optimize.fmin_slsqp(func, x0, f_eqcons=f_eqcons, fprime_eqcons=fprime_eqcons,
                                                                fprime=fprime, acc=mvi.tolerance, iter=1000,
                                                                bounds=bounds, iprint=0, full_output=True)
 
@@ -87,9 +124,9 @@ def root_solve_release(mvi, j):
   
     mvi.set_midpoint()
     mvi.p1 = D2L2(mvi)
-    mvi.q1 = x_opt[:n]
+    mvi.q1 = x_opt[:nd]
     mvi.t1 = x_opt[-1]
-    mvi.lambda0 = x_opt[n:(n+m)]
+    mvi.lambda0 = x_opt[nd:(nd+m)]
 
     return {'t': mvi.t1, 'q': mvi.q1, 'p': mvi.p1, 'lam0': mvi.lambda0}
 

src/util.py
@@ -1,114 +1,82 @@
 import numpy as np
 
-
-def set_system_state(mvi, i):
-    """ Sets the state of sytem to either 1 or 2. """
-    if i==1:
-        mvi.system.set_state(q=mvi.q1, t=mvi.t1)
-    if i==2:
-        mvi.system.set_state(q=mvi.q2, t=mvi.t2)
-
+"""
+ The slot derivatives refer to the arguments of the discrete Lagrangian:
+ L2(q1, q2, t1, t2) = dt*L((q1+q2)/2, (q2-q1)/dt, (t1+t2)/2)
+ and the discrete forcing:
+ f2_minus(q1, q2, t1, t2) = dt*f((q1+q2)/2, (q2-q1)/dt, (t1+t2)/2).
+"""
 
 def D1L2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([0.5*mvi.system.L_dq(q)*dt-mvi.system.L_ddq(q)
-                     for q in mvi.system.dyn_configs])
-
+    return np.array([0.5*mvi.system.L_dq(q)*dt-mvi.system.L_ddq(q) for q in mvi.system.dyn_configs])
 
 def D2L2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([0.5*mvi.system.L_dq(q)*dt+mvi.system.L_ddq(q)
-                    for q in mvi.system.dyn_configs])
+    return np.array([0.5*mvi.system.L_dq(q)*dt+mvi.system.L_ddq(q) for q in mvi.system.dyn_configs])
 
+def D4L2(mvi):
+    return mvi.system.L()-np.dot(mvi.system.dq[:mvi.nd], [mvi.system.L_ddq(q) for q in mvi.system.dyn_configs])
+
+def D3L2(mvi):
+    return -mvi.system.L()+np.dot(mvi.system.dq[:mvi.nd], [mvi.system.L_ddq(q) for q in mvi.system.dyn_configs])    
 
 def D2D1L2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([[0.25*dt*mvi.system.L_dqdq(q1,q2)
-                     +0.5*mvi.system.L_ddqdq(q1,q2)
-                     -0.5*mvi.system.L_ddqdq(q2,q1)
-                     -1.0/dt*mvi.system.L_ddqddq(q1,q2)
-                    for q1 in mvi.system.dyn_configs]
-                    for q2 in mvi.system.dyn_configs])
-
+    return np.array([[0.25*dt*mvi.system.L_dqdq(q1,q2)+0.5*mvi.system.L_ddqdq(q1,q2)
+                     -0.5*mvi.system.L_ddqdq(q2,q1)-1.0/dt*mvi.system.L_ddqddq(q1,q2)
+                    for q1 in mvi.system.dyn_configs] for q2 in mvi.system.dyn_configs])
 
 def fm2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([sum([force.f(q)*dt
-                   for force in mvi.system.forces])
-                   for q in mvi.system.dyn_configs])
-
+    return np.array([sum([force.f(q)*dt for force in mvi.system.forces]) for q in mvi.system.dyn_configs])
 
 def D2fm2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([[sum([0.5*force.f_dq(q1,q2)*dt
-                    +force.f_ddq(q1,q2)
-                    for force in mvi.system.forces])
-                    for q2 in mvi.system.dyn_configs]
-                    for q1 in mvi.system.dyn_configs])
+    return np.array([[sum([0.5*force.f_dq(q1,q2)*dt+force.f_ddq(q1,q2) for force in mvi.system.forces])
+                    for q2 in mvi.system.dyn_configs] for q1 in mvi.system.dyn_configs])
 
-
-def D4D1L2(mvi):
+def D1fm2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([[0.5*mvi.system.L_dq(q1)
-                      -0.5*np.dot(mvi.system.dq, [mvi.system.L_ddqdq(q2,q1)
-                                 for q2 in mvi.system.configs])
-                      +1.0/dt*np.dot(mvi.system.dq,[mvi.system.L_ddqddq(q1,q2)
-                                 for q2 in mvi.system.configs])]
-                     for q1 in mvi.system.dyn_configs])
+    return np.array([[sum([0.5*force.f_dq(q1,q2)*dt-force.f_ddq(q1,q2) for force in mvi.system.forces])
+                    for q2 in mvi.system.dyn_configs] for q1 in mvi.system.dyn_configs])    
 
-    
 def D4fm2(mvi):
     dt = mvi.t2-mvi.t1
     return np.array([[sum([force.f(q1) for force in mvi.system.forces])
-                      -np.dot(mvi.system.dq, [sum([force.f_dq(q1,q2)
-                    for force in mvi.system.forces])
-                    for q2 in mvi.system.configs])]
-                    for q1 in mvi.system.dyn_configs])
-
-
-def D4L2(mvi):
-    return mvi.system.L()-np.dot(mvi.system.dq,
-            [mvi.system.L_ddq(q) for q in mvi.system.configs])
-
-
-def D3L2(mvi):
-    return -mvi.system.L()+np.dot(mvi.system.dq,
-            [mvi.system.L_ddq(q) for q in mvi.system.configs])
-
+                      -np.dot(mvi.system.dq[:mvi.nd],
+                      [sum([force.f_dq(q1,q2) for force in mvi.system.forces]) for q2 in mvi.system.dyn_configs])]
+                     for q1 in mvi.system.dyn_configs])
+    
+def D4D1L2(mvi):
+    dt = mvi.t2-mvi.t1
+    return np.array([[0.5*mvi.system.L_dq(q1)
+                      -0.5*np.dot(mvi.system.dq[:mvi.nd], [mvi.system.L_ddqdq(q2,q1) for q2 in mvi.system.dyn_configs])
+                      +1.0/dt*np.dot(mvi.system.dq[:mvi.nd], [mvi.system.L_ddqddq(q1,q2) for q2 in mvi.system.dyn_configs])]
+                     for q1 in mvi.system.dyn_configs])
 
 def D2D3L2(mvi):
     dt = mvi.t2-mvi.t1
-    return np.array([-0.5*mvi.system.L_dq(q1)
-                    +np.dot(mvi.system.dq, [0.5*mvi.system.L_ddqdq(q1,q2)
-		            +1.0/dt*mvi.system.L_ddqddq(q1,q2) for q2 in mvi.system.configs])
+    return np.array([-0.5*mvi.system.L_dq(q1) + np.dot(mvi.system.dq[:mvi.nd], [0.5*mvi.system.L_ddqdq(q1,q2)
+		            +1.0/dt*mvi.system.L_ddqddq(q1,q2) for q2 in mvi.system.dyn_configs])
                     for q1 in mvi.system.dyn_configs])
-
 
 def h(mvi):
     return np.array([c.h() for c in mvi.system.constraints])
 
-
 def Dh(mvi):
-    # This needs a transpose to handle the case of no constraints,
-    # where the shapes of the empty arrays need to match.
-    return np.transpose(np.array([[c.h_dq(q) for c in mvi.system.constraints]
-                                   for q in mvi.system.dyn_configs]))
+    dhq = np.array([[c.h_dq(q) for q in mvi.system.dyn_configs] for c in mvi.system.constraints])
+    return np.reshape(dhq, (mvi.nc, mvi.nd))
 
-    
 def phi(mvi):
-    mvi.surface.system.set_state(q=mvi.system.q, t=mvi.system.t)
     return mvi.surface.phi()
 
-
 def Dphi(mvi, q_i=None):
-    mvi.surface.system.set_state(q=mvi.system.q, t=mvi.system.t)
     if q_i is not None:
         return mvi.surface.h_dq(q_i)
-    return np.array([[mvi.surface.phi_dq(q)] for q in mvi.system.dyn_configs])
-
+    return np.array([mvi.surface.phi_dq(q) for q in mvi.system.dyn_configs])
 
 def D2h(mvi):
-    mvi.surface.system.set_state(q=mvi.system.q, t=mvi.system.t)
     Dh = np.zeros(mvi.nc)
     for i in range(mvi.nc):
         constraint = mvi.system.get_constraint(i)
@@ -117,7 +85,15 @@ def D2h(mvi):
         except:
             pass
     return Dh
-    
+
+def set_system_state(mvi, i):
+    """ Sets the state of sytem to either 1 or 2. """
+    if i==0:
+        mvi.system.set_state(q=mvi.q0, t=mvi.t0)
+    if i==1:
+        mvi.system.set_state(q=mvi.q1, t=mvi.t1)
+    if i==2:
+        mvi.system.set_state(q=mvi.q2, t=mvi.t2)     
     
 def save_state(mvi):
     """ Saves the integrator's states in a dictionary. """
@@ -126,7 +102,6 @@ def save_state(mvi):
              't0': mvi.t0, 't1': mvi.t1, 't2': mvi.t2,
              'lambda0': mvi.lambda0, 'lambda1': mvi.lambda1}
     return state
-
 
 def restore_state(mvi, state):
     """ Resets the integrator's state from a dictionary. """